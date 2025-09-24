0a051537c8c671460e67fa22be9b2ca99408e089
Ethan Sueiro <etalvasu360@gmail.com>
2025-09-24 15:20:05 -0400

    .

--- ToDo/Frame.cpp
+++ ToDo/Frame.cpp
@@ -10,3 +10,8 @@ using namespace std;
 wxBEGIN_EVENT_TABLE(Frame, wxWindow)
 wxEND_EVENT_TABLE();
 
+//Frame::Frame() : wxWindow(nullptr, wxID_ANY, "Window", wxPoint(200, 200), wxSize(400, 600))
+//{
+
+
+//}
