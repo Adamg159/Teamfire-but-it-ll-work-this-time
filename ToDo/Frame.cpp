7319cfad21f769d6d815af52496a17be61ed9b35
Ethan Sueiro <etalvasu360@gmail.com>
2025-09-24 15:13:28 -0400

    window progress

--- ToDo/Frame.cpp
+++ ToDo/Frame.cpp
@@ -1,1 +1,12 @@
 #include "Frame.h"
+#include "App.h"
+#include <wx/wx.h>
+#include "wx/event.h"
+#include <wx/tokenzr.h>
+#include <sstream>
+
+using namespace std;
+
+wxBEGIN_EVENT_TABLE(Frame, wxWindow)
+wxEND_EVENT_TABLE();
+
--- ToDo/Frame.h
+++ ToDo/Frame.h
@@ -4,14 +4,19 @@
 #include "wx/event.h"
 #include "wx/sizer.h"
 
+using namespace std;
+
 class Frame : public wxWindow
 {
 
 private:
+	wxBoxSizer* FrameSizer;
+	wxTextCtrl* FrameText;
 
 
 public:
-
+	Frame();
+	wxDECLARE_EVENT_TABLE();
 
 };
 
